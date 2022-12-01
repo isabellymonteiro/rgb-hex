bb5e75bd72169484d9ef7d18099f895df6aea99c
Isabelly Monteiro <isabelly.monteiro@toptal.com>
2022-11-30 21:46:04 -0300

    Create package

---
+++ index.js
@@ -0,0 +1,17 @@
+function colorToHex(color) {
+  const hex = color.toString(16)
+  return hex.length === 1 ? '0' + hex : hex
+}
+
+export default function rgbToHex(red, green, blue) {
+  return `#${colorToHex(red) + colorToHex(green) + colorToHex(blue)}`
+}
+
+export default function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null
+}
